4f756bb43dccedcb091b7da70e18a91e481a6876
Sree Hari <98310399+Srehrix@users.noreply.github.com>
2022-11-30 16:41:42 +0530

    Update __init__.py

--- ChannelBot/database/__init__.py
+++ ChannelBot/database/__init__.py
@@ -2,7 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from Config import DATABASE_URL
+from aiohttp import web
+from .route import routes
 
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
 
 def start() -> scoped_session:
     engine = create_engine(DATABASE_URL)
